1afe228ea0102bfbfda73584d1e2121a5db31660
Omose1994 <81728852+Omose1994@users.noreply.github.com>
2021-04-02 00:57:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var itemPrice1 = 2;
+var amountOfMoney = 200;
+let numberOfFriends = 3;
+let AgeInYears = 27;
+let firstName = "Omose";
+const lastName = "Edobor";
+const middleInitial = "R";
+var change = "Your Change is ";
+
+let fullName = firstName + " " + middleInitial + " " + lastName;
+var newAmount = (amountOfMoney - itemPrice1);
+
+
+console.log(change + newAmount);
+console.log('The number of friends I made each year is ' + AgeInYears/numberOfFriends);
+console.log(fullName);
+console.log("The price of the item is" + " " + itemPrice1);
+console.log("The amount of money in my wallet is" + " " + amountOfMoney);
+console.log("The number of friends I have are" + " " + numberOfFriends);
+console.log("I am" + " " + AgeInYears + " " + "years old");
+
